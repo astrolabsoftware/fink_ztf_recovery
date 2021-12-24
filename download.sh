8a8c0018c0218086ddbe438ae6bc8c7b61d93dee
Julien <peloton@lal.in2p3.fr>
2021-12-24 09:35:27 +0100

    redirect downloads

--- download.sh
+++ download.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+# Copyright 2021 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 list_of_files=`/usr/bin/ls -1 uris | grep .txt`
 
 for file in ${list_of_files[@]}; do
-    echo $file
-    #aria2c -x16 -s10 -i $file
+    #echo $file
+    aria2c -x16 -s10 -i $file -d /spark-dir/tars
 done
-
-#aria2c -x16 -s10 -i uri-list.txt
